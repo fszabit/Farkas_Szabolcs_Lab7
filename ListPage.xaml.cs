7ebbd6a17aa7739f5d8fbd0df6ac4e68d0aeb5be
Szabolcs Tamas Farkas <tszabolcs26@yahoo.com>
2023-01-07 15:45:30 +0200

    Laboratorul 9

--- ListPage.xaml.cs
+++ ListPage.xaml.cs
@@ -8,7 +8,7 @@ public partial class ListPage : ContentPage
 	{
 		InitializeComponent();
 	}
-    async void OnSaveButtonClicked(object sender, EventArgs e)
+   async void OnSaveButtonClicked(object sender, EventArgs e)
     {
         var slist = (ShopList)BindingContext;
         slist.Date = DateTime.UtcNow;
@@ -20,5 +20,21 @@ public partial class ListPage : ContentPage
         var slist = (ShopList)BindingContext;
         await App.Database.DeleteShopListAsync(slist);
         await Navigation.PopAsync();
+    }
+    async void OnChooseButtonClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ProductPage((ShopList)
+       this.BindingContext)
+        {
+            BindingContext = new Product()
+        });
+
+    }
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        var shopl = (ShopList)BindingContext;
+
+        listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
     }
 }
