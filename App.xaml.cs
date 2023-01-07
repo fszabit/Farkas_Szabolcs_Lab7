79056155f2fb0c65da07940766e068ff15a0e401
Szabolcs Tamas Farkas <tszabolcs26@yahoo.com>
2023-01-07 11:41:49 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Farkas_Szabolcs_Lab7;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Farkas_Szabolcs_Lab7;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
